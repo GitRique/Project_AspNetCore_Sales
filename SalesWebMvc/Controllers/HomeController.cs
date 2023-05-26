62ecdf1d32d4314c73779f054d6196ed52480459
Henrique Lopes <hsilvalopes9@gmail.com>
2023-05-25 23:42:33 -0300

    End Project 1.1 - New design

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -25,7 +25,7 @@ namespace SalesWebMvc.Controllers
 
         public IActionResult Contact()
         {
-            ViewData["Message"] = "Your contact page.";
+            ViewData["Message"] = " ";
 
             return View();
         }
